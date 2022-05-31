25edeb1f027de082edc36b23568b306bfc687cc9
Jennifer Chen <106080396+jenniferchenjy@users.noreply.github.com>
2022-05-31 22:29:27 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruit_to_show)
 
 #create the repeatable code block
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_resposne=requests.get("https://fruityvice.com/api/fruit"+this_fruit_choice)
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit"+this_fruit_choice)
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
@@ -52,12 +52,18 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('from streamlit')")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#snowflake related functions
+def get_fruit_load_list():
+    with my_cur = my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.fruit_load_list")
+        return my_data_rows = my_cur.fetchall()
+
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 fruit_choice_2=streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding', fruit_choice_2)
