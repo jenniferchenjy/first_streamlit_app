ab2cd4e614534015e8b33e187b77de73ee634fed
Jennifer Chen <106080396+jenniferchenjy@users.noreply.github.com>
2022-05-31 22:33:08 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,6 @@ except URLError as e:
   
 streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
 streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
